0f9947ef0845e11f5014b3aed2016f6923e3fdd8
BYAM777 <gysius423@gmail.com>
2025-01-21 17:46:06 +0200

    Automated creation of first ship

--- battleship-backend/server.js
+++ battleship-backend/server.js
@@ -5,9 +5,15 @@ const app = express();
 app.use(cors());
 
 const generateShip = () => {
+    const orientation = Math.random() > 0.5 ? 'horizontal' : 'vertical';
+    const startX = Math.ceil(Math.random() * (orientation === 'horizontal' ? 5 : 9));
+    const startY = Math.ceil(Math.random() * (orientation === 'vertical' ? 5 : 9));
+
     const ship = [];
     for(let i = 0; i < 5; i++){
-        ship.push({x: 1, y: i})
+        ship.push({x: orientation === 'horizontal' ? startX + i : startX,
+                   y: orientation === 'vertical' ? startY +i : startY
+        })
     }
     return ship;    
 }
--- battleship-frontend/src/App.js
+++ battleship-frontend/src/App.js
@@ -12,13 +12,25 @@ const Battleship = () => {
     .then((data) => setShip(data));
   }, []);
   
-  console.log(ship);
-
   const grid = Array.from({length: 10}, (_, x) =>
     Array.from({length: 10}, (_, y) => ({x, y}))
   );
 
   const isPartOfShip = (x, y ) => ship.some((block) => block.x === x && block.y ===y);
+
+
+  const orientation = Math.random() > 0.5 ? 'horizontal' : 'vertical';
+  const startX = Math.ceil(Math.random() * (orientation === 'horizontal' ? 5 : 9));
+  const startY = Math.ceil(Math.random() * (orientation === 'vertical' ? 5 : 9));
+
+  for(let i = 0; i < 10000; i++){
+    //console.log(i);
+    if(startX > 9 || startY > 9){
+      console.log("Out of bounds");
+    }
+  }
+
+  //console.log(ship);
 
   return (
     <div className="App">
